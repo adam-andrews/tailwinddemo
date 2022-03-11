7e3ee4eb5c20e6cf64e4716e613526d4012602f7
Adam Andrews <andrewsadam07@gmail.com>
2022-03-11 16:58:05 +0000

    Configured JIT timer

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  mode: 'jit',
+   content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+   ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
